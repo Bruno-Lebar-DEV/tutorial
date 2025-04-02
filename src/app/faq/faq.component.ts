497d6a897d482b9961843d23fcfbd064569b1581
Bruno Lebar <brunolebarcarmo@gmail.com>
2025-04-02 16:05:07 -0300

    FAQ Page

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ViewChild } from '@angular/core';
+import { PoAccordionItemComponent, PoModule, PoTagType } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-faq',
-  imports: [],
+  imports: [PoModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
-export class FaqComponent {
+export class FaqComponent implements AfterContentInit {
+  @ViewChild(PoAccordionItemComponent, { static: true })
+  questionOne!: PoAccordionItemComponent;
 
+
+  title: string = 'PO UI FAQ';
+  description: string = 'Frequently Asked Questions about PO UI.';
+  successTag: PoTagType = PoTagType.Success;
+  warningTag: PoTagType = PoTagType.Warning;
+  dangerTag: PoTagType = PoTagType.Danger;
+  infoTag: PoTagType = PoTagType.Info;
+
+
+  ngAfterContentInit() {
+    this.questionOne.expand();
+  }
 }
